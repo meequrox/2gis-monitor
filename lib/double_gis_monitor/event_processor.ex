e4076a84bccbcabc770207ddac0928122e53fa06
meequrox <117393211+meequrox@users.noreply.github.com>
2024-04-14 15:44:55 +0700

    Fix DB cleanup

--- lib/double_gis_monitor/event_processor.ex
+++ lib/double_gis_monitor/event_processor.ex
@@ -2,8 +2,6 @@ defmodule EventProcessor do
   use Agent
   require Logger
 
-  import Ecto.Query, only: [from: 2]
-
   @outdate_hours 24
 
   #############
@@ -30,6 +28,7 @@ defmodule EventProcessor do
     }
   end
 
+  @spec start_link([]) :: {:error, any()} | {:ok, pid()}
   def start_link([]) do
     Agent.start_link(__MODULE__, :init, [], name: __MODULE__)
   end
@@ -39,61 +38,35 @@ defmodule EventProcessor do
   end
 
   def process(events) do
-    ensure_db_cleaned(events)
-    # TODO(0): insert new events in db
-    # TODO(1):
+    state = Agent.get(__MODULE__, fn map -> map end)
+    datetime_now = DateTime.utc_now()
+
+    new_state =
+      case state.first_run or
+             DateTime.diff(datetime_now, state.last_cleanup, :hour) > @outdate_hours do
+        true ->
+          Logger.info("Database cleanup started")
+
+          case DoubleGisMonitor.Repo.cleanup(events, @outdate_hours) do
+            {:ok, n} ->
+              Logger.info("Deleted #{n} old database entries")
+              %{state | first_run: false, last_cleanup: datetime_now}
+
+            err ->
+              Logger.warning("Unknown result from cleanup function: #{err}")
+              state
+          end
+
+        false ->
+          %{state | first_run: false}
+      end
+
+    Agent.update(__MODULE__, fn _old_state -> new_state end)
+
+    # TODO(0): Insert new events in DB
   end
 
   #############
   ## Private
   #############
-
-  defp ensure_db_cleaned(events) do
-    %{first_run: first_run, last_cleanup: last_cleanup} = Agent.get(__MODULE__, fn map -> map end)
-
-    case first_run or DateTime.diff(DateTime.utc_now(), last_cleanup) > @outdate_hours do
-      true ->
-        Logger.info("Database cleanup started")
-
-        query = from(e in "events", select: {e.uuid, e.datetime})
-        outdated_db_events = find_outdated_db_events(DoubleGisMonitor.Repo.all(query))
-
-        f_cleanup =
-          fn %{:uuid => outdated_uuid} = s, acc ->
-            f_persist =
-              fn %{:uuid => uuid} ->
-                uuid === outdated_uuid
-              end
-
-            case Enum.find(events, nil, f_persist) do
-              nil ->
-                DoubleGisMonitor.Repo.delete(s, returning: false)
-                acc + 1
-
-              _ ->
-                acc
-            end
-          end
-
-        n = Enum.reduce(outdated_db_events, 0, f_cleanup)
-        Logger.info("Deleted #{n} old database entries")
-
-        # TODO(1): update last_cleanup in state
-        {:ok, :cleaned}
-
-      false ->
-        {:ok, :skipped}
-    end
-  end
-
-  defp find_outdated_db_events(db_events) do
-    current_datetime = DateTime.utc_now()
-
-    f =
-      fn %{:datetime => event_datetime} ->
-        DateTime.diff(current_datetime, event_datetime) > @outdate_hours
-      end
-
-    Enum.filter(db_events, f)
-  end
 end
--- lib/double_gis_monitor/repo.ex
+++ lib/double_gis_monitor/repo.ex
@@ -2,4 +2,51 @@ defmodule DoubleGisMonitor.Repo do
   use Ecto.Repo,
     otp_app: :double_gis_monitor,
     adapter: Ecto.Adapters.Postgres
+
+  import Ecto.Query, only: [from: 2]
+
+  #############
+  ## API
+  #############
+
+  def cleanup(events, hours) do
+    query = from(e in "events", select: {e.uuid, e.datetime})
+    outdated_db_events = find_outdated_events(DoubleGisMonitor.Repo.all(query), hours)
+
+    f_cleanup =
+      fn %{:uuid => outdated_uuid} = s, acc ->
+        f_persist =
+          fn %{:uuid => uuid} ->
+            uuid === outdated_uuid
+          end
+
+        case Enum.find(events, nil, f_persist) do
+          nil ->
+            DoubleGisMonitor.Repo.delete(s, returning: false)
+            acc + 1
+
+          _ ->
+            acc
+        end
+      end
+
+    n = Enum.reduce(outdated_db_events, 0, f_cleanup)
+
+    {:ok, n}
+  end
+
+  #############
+  ## Private
+  #############
+
+  defp find_outdated_events(db_events, hours) do
+    current_datetime = DateTime.utc_now()
+
+    f =
+      fn %{:datetime => event_datetime} ->
+        DateTime.diff(current_datetime, event_datetime, :hours) > hours
+      end
+
+    Enum.filter(db_events, f)
+  end
 end
