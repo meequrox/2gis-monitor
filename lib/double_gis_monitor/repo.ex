ef4a983cbf6f7e8537fb23a8fd7205be612da54f
meequrox <117393211+meequrox@users.noreply.github.com>
2024-04-16 01:11:38 +0700

    Repo: implement insert of new events

--- lib/double_gis_monitor/repo.ex
+++ lib/double_gis_monitor/repo.ex
@@ -3,24 +3,24 @@ defmodule DoubleGisMonitor.Repo do
     otp_app: :double_gis_monitor,
     adapter: Ecto.Adapters.Postgres
 
-  import Ecto.Query, only: [from: 2]
+  require Logger
 
   #############
   ## API
   #############
 
   def cleanup(events, hours) do
-    query = from(e in "events", select: {e.uuid, e.datetime})
-    outdated_db_events = find_outdated_events(DoubleGisMonitor.Repo.all(query), hours)
+    outdated_db_events =
+      DoubleGisMonitor.Event |> DoubleGisMonitor.Repo.all() |> find_outdated_events(hours)
 
-    f_cleanup =
+    reduce_fn =
       fn %{:uuid => outdated_uuid} = s, acc ->
-        f_persist =
+        find_fn =
           fn %{:uuid => uuid} ->
             uuid === outdated_uuid
           end
 
-        case Enum.find(events, nil, f_persist) do
+        case Enum.find(events, nil, find_fn) do
           nil ->
             DoubleGisMonitor.Repo.delete(s, returning: false)
             acc + 1
@@ -30,23 +30,54 @@ defmodule DoubleGisMonitor.Repo do
         end
       end
 
-    n = Enum.reduce(outdated_db_events, 0, f_cleanup)
+    {:ok, Enum.reduce(outdated_db_events, 0, reduce_fn)}
+  end
 
-    {:ok, n}
+  @spec insert_new(any()) :: list()
+  def insert_new(events) do
+    filter_fn = fn e -> ensure_inserted?(e) end
+    Enum.filter(events, filter_fn)
   end
 
   #############
   ## Private
   #############
 
+  defp ensure_inserted?(
+         %{:uuid => uuid, :likes => likes, :dislikes => dislikes, :attachments_count => attch_cnt} =
+           e
+       ) do
+    case DoubleGisMonitor.Repo.get(DoubleGisMonitor.Event, uuid) do
+      nil ->
+        DoubleGisMonitor.Repo.insert(e)
+        true
+
+      db_event ->
+        case db_event.likes !== likes or db_event.dislikes !== dislikes or
+               db_event.attachments_count !== attch_cnt do
+          true ->
+            DoubleGisMonitor.Repo.update(e)
+            true
+
+          false ->
+            false
+        end
+    end
+  end
+
+  defp ensure_inserted?(%{} = e) do
+    Logger.warning("Event #{inspect(e)} doest not contain required keys!")
+    false
+  end
+
   defp find_outdated_events(db_events, hours) do
     current_datetime = DateTime.utc_now()
 
-    f =
+    filter_fn =
       fn %{:datetime => event_datetime} ->
-        DateTime.diff(current_datetime, event_datetime, :hours) > hours
+        DateTime.diff(current_datetime, event_datetime, :hour) > hours
       end
 
-    Enum.filter(db_events, f)
+    Enum.filter(db_events, filter_fn)
   end
 end
