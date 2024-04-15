a3def6bdd982f681815be3bc0d55c771ece50708
meequrox <117393211+meequrox@users.noreply.github.com>
2024-04-16 01:11:10 +0700

    EventProcessor: convert events to DB entries representation

--- lib/double_gis_monitor/event_processor.ex
+++ lib/double_gis_monitor/event_processor.ex
@@ -41,19 +41,21 @@ defmodule DoubleGisMonitor.EventProcessor do
     state = Agent.get(__MODULE__, fn map -> map end)
     datetime_now = DateTime.utc_now()
 
+    converted_events = events |> Enum.map(fn e -> convert_event_to_db(e) end)
+
     new_state =
       case state.first_run or
              DateTime.diff(datetime_now, state.last_cleanup, :hour) > @outdate_hours do
         true ->
           Logger.info("Database cleanup started")
 
-          case DoubleGisMonitor.Repo.cleanup(events, @outdate_hours) do
+          case DoubleGisMonitor.Repo.cleanup(converted_events, @outdate_hours) do
             {:ok, n} ->
               Logger.info("Deleted #{n} old database entries")
               %{state | first_run: false, last_cleanup: datetime_now}
 
             err ->
-              Logger.warning("Unknown result from cleanup function: #{err}")
+              Logger.warning("Unknown result from cleanup function: #{inspect(err)}")
               state
           end
 
@@ -61,14 +63,49 @@ defmodule DoubleGisMonitor.EventProcessor do
           %{state | first_run: false}
       end
 
-    Agent.update(__MODULE__, fn _old_state -> new_state end)
+    Agent.update(__MODULE__, fn _ -> new_state end)
 
-    Logger.info("Processed #{Enum.count(events)} events!")
+    new_events =
+      converted_events |> DoubleGisMonitor.Repo.insert_new()
 
-    # TODO(0): Insert new events in DB
+    # Send new events to dispatcher module
+
+    Logger.info(
+      "Processed #{Enum.count(events)} events, inserted (or updated) #{Enum.count(new_events)} events!"
+    )
   end
 
   #############
   ## Private
   #############
+
+  defp convert_event_to_db(
+         %{
+           "id" => id,
+           "timestamp" => ts,
+           "type" => type,
+           "user" => user_info,
+           "location" => %{"coordinates" => [lon, lat]},
+           "feedbacks" => %{"likes" => likes, "dislikes" => dislikes},
+           "attachments_count" => atch_count,
+           "attachments_list" => atch_list
+         } = e
+       ) do
+    %DoubleGisMonitor.Event{
+      uuid: id,
+      datetime: DateTime.from_unix!(ts),
+      type: type,
+      username: Map.get(user_info, "name"),
+      coordinates: %{:lat => lat, :lon => lon},
+      comment: Map.get(e, "comment"),
+      likes: likes,
+      dislikes: dislikes,
+      attachments_count: atch_count,
+      attachments_list: atch_list
+    }
+  end
+
+  defp convert_event_to_db(_) do
+    %DoubleGisMonitor.Event{}
+  end
 end
