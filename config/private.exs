ec6938fb0d2131be51ef1115d00deb0d2217eb1e
meequrox <117393211+meequrox@users.noreply.github.com>
2024-04-30 02:11:22 +0700

    Update private config

--- config/private.exs
+++ config/private.exs
@@ -1,6 +1,6 @@
 import Config
 
-config(:ex_gram,
-  method: :polling,
-  token: "YOUR TOKEN HERE"
-)
+config(:telegex, token: "TELEGRAM_BOT_TOKEN_HERE")
+
+# ID of the Telegram channel in which the bot is an administrator
+config(:double_gis_monitor, :dispatch, channel_id: -1)
