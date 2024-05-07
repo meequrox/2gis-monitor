4210ff3ee5e631b144207b624e9cd94d979cb5d3
meequrox <117393211+meequrox@users.noreply.github.com>
2024-05-08 03:17:30 +0700

    Bump version 1.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DoubleGisMonitor.MixProject do
   def project do
     [
       app: :double_gis_monitor,
-      version: "1.0.1",
+      version: "1.2.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
