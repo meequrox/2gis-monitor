7581e23b6aecc29abb3e2e7bc2f25e23c31e2178
meequrox <117393211+meequrox@users.noreply.github.com>
2024-05-09 02:15:21 +0700

    Bump Telegex dependency

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule DoubleGisMonitor.MixProject do
       {:ecto_sql, "~> 3.11"},
       {:postgrex, "~> 0.17.5"},
       {:time_zone_info, "~> 0.7.2"},
-      {:telegex, "~> 1.6"},
+      {:telegex, "~> 1.7"},
       {:logger_file_backend, "~> 0.0.14"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
