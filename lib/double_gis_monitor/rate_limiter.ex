844a866fd04a91564335bb9d19ed32bbd2a101a5
meequrox <117393211+meequrox@users.noreply.github.com>
2024-05-09 02:13:06 +0700

    Add RateLimiter doc

--- lib/double_gis_monitor/rate_limiter.ex
+++ lib/double_gis_monitor/rate_limiter.ex
@@ -1,6 +1,16 @@
 defmodule DoubleGisMonitor.RateLimiter do
   @moduledoc """
-  TODO
+  A module that is responsible for "sleeping" the calling process for a certain time
+  in order to avoid blocking by third-party web services or to eliminate conflicts within the application.
+
+  This is a rather primitive implementation of a "just to make it work" limiter.
+  The names of the modules that can access the limiter are known in advance.
+  For each module, an arbitrary set of actions is supported that determine the waiting time.
+
+  It is also important WHEN exactly to fall asleep.
+  It is possible to call a limiter before executing the action itself, in which case its return value does not matter.
+  A limiter can be called after executing the request, with the value it should return.
+  This is done for the convenience of returning a value in the main function using pipes.
   """
 
   require Logger
@@ -16,27 +26,29 @@ defmodule DoubleGisMonitor.RateLimiter do
     }
   end
 
-  defp get_timeout(module, action, multiplier) do
-    base_timeout = timeout_map() |> Map.get(module, %{}) |> Map.get(action, @default_timeout)
-
-    base_timeout * multiplier
-  end
-
-  defp sleep({module, action, times}, where) when is_binary(where) do
-    timeout = get_timeout(module, action, times)
-
-    Logger.info("Sleeping #{timeout}ms for #{inspect(module)} #{where} #{action}.")
-    Process.sleep(timeout)
-  end
-
+  @spec sleep_after(any(), atom(), atom(), integer()) :: any()
   def sleep_after(return_value, module, action, times \\ 1)
       when is_atom(module) and is_atom(action) and is_integer(times) do
     sleep({module, action, times}, "AFTER")
     return_value
   end
 
+  @spec sleep_before(atom(), atom(), integer()) :: :ok
   def sleep_before(module, action, times \\ 1)
       when is_atom(module) and is_atom(action) and is_integer(times) do
     sleep({module, action, times}, "BEFORE")
+  end
+
+  defp get_timeout(module, action, multiplier) do
+    base_timeout = timeout_map() |> Map.get(module, %{}) |> Map.get(action, @default_timeout)
+
+    base_timeout * multiplier
+  end
+
+  defp sleep({module, action, times}, when_) when is_binary(when_) do
+    timeout = get_timeout(module, action, times)
+
+    Logger.info("Sleeping #{timeout}ms for #{inspect(module)} #{when_} #{action}.")
+    Process.sleep(timeout)
   end
 end
