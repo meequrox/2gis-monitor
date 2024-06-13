5966a119bbecfc7bc210c11d40a302d032d79025
meequrox <117393211+meequrox@users.noreply.github.com>
2024-06-14 02:09:55 +0700

    Fix log rotating bug

--- config/config.exs
+++ config/config.exs
@@ -8,13 +8,19 @@ timezone_db =
     {:win32, _name} -> Tz.TimeZoneDatabase
   end
 
-log_opts = %{
-  dir: "log",
+logger_shared_opts = [
   format: "[$date] [$time] [$level] $metadata: $message\n",
-  metadata: [:registered_name, :pid, :mfa],
-  truncate: :infinity,
-  rotate: %{max_bytes: 104_857_600, keep: 3}
-}
+  metadata: [:registered_name, :pid, :mfa]
+]
+
+logger_file_opts =
+  [
+    truncate: :infinity,
+    rotate: %{max_bytes: 104_857_600, keep: 3},
+    max_file_num: 3
+  ] ++ logger_shared_opts
+
+logger_file_dir = "log"
 
 config(:double_gis_monitor,
   ecto_repos: [DoubleGisMonitor.Database.Repo]
@@ -26,40 +32,40 @@ config(:telegex, caller_adapter: {HTTPoison, [recv_timeout: 5 * 1000]})
 
 config(:elixir, :time_zone_database, timezone_db)
 
-config(:logger, :console,
-  format: log_opts.format,
-  metadata: log_opts.metadata,
-  level: :info,
-  colors: [info: :light_green]
+config(
+  :logger,
+  :console,
+  [
+    level: :info,
+    colors: [info: :light_green]
+  ] ++ logger_shared_opts
 )
 
-# BUG: Log rotating does not work
-
-config(:logger, :debug_log,
-  format: log_opts.format,
-  metadata: log_opts.metadata,
-  truncate: log_opts.truncate,
-  rotate: log_opts.rotate,
-  level: :debug,
-  path: Path.join(log_opts.dir, "debug.log")
+config(
+  :logger,
+  :debug_log,
+  [
+    level: :debug,
+    path: Path.join(logger_file_dir, "debug.log")
+  ] ++ logger_file_opts
 )
 
-config(:logger, :info_log,
-  format: log_opts.format,
-  metadata: log_opts.metadata,
-  truncate: log_opts.truncate,
-  rotate: log_opts.rotate,
-  level: :info,
-  path: Path.join(log_opts.dir, "info.log")
+config(
+  :logger,
+  :info_log,
+  [
+    level: :info,
+    path: Path.join(logger_file_dir, "info.log")
+  ] ++ logger_file_opts
 )
 
-config(:logger, :error_log,
-  format: log_opts.format,
-  metadata: log_opts.metadata,
-  truncate: log_opts.truncate,
-  rotate: log_opts.rotate,
-  level: :error,
-  path: Path.join(log_opts.dir, "error.log")
+config(
+  :logger,
+  :error_log,
+  [
+    level: :error,
+    path: Path.join(logger_file_dir, "error.log")
+  ] ++ logger_file_opts
 )
 
 import_config("#{env}.exs")
